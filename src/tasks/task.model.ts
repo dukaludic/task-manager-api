b26670151afcc2d24720239519a1582c9b2f6d7d
dukaludic <dukaludic@gmail.com>
2022-03-09 03:19:13 +0100

    task model

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -4,18 +4,18 @@ export const TaskSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     project_id: { type: String, required: true },
-    assigned_users: { type: Array, required: true },
+    assigned_users: { type: Array, required: false },
 
     project_manager_id: { type: String, required: false },
-    sub_tasks: { type: Array, required: true },
+    sub_tasks: { type: Array, required: false },
     status: {
       type: String,
       enum: ['to_do', 'in_progress', 'in_review', 'done'],
       required: true,
     },
     description: { type: String, required: false },
-    created_by: { type: String, required: true },
-    creation_time: { type: Date, required: true },
+    created_by: { type: String, required: false },
+    creation_time: { type: Date, required: false },
     due_date: { type: Date, required: false },
   },
   { versionKey: false },
